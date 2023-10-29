6ae7c33bf4d636b7d58e7e582abb2643f26cd179
Shyam Sharki <ssk66221@gmail.com>
2023-10-29 06:55:15 +0545

    merged array

--- MergedArray.js
+++ MergedArray.js
@@ -1,19 +1,17 @@
 function mergedArray(arr1,arr2){
-    //Step1 :merged the array 
-    let newArray=[...arr1,...arr2];
-  
-    let uniqueElemen=[];
 
-    for(let i=0;i<newArray.length;i++){
+    let  newArr=[...arr1,...arr2];
+
+    
+    let arr=newArr.filter((ele,index)=>{
 
 
-        if(uniqueElemen.indexOf(newArray[i])===-1){
+        return newArr.indexOf(ele)===index
 
-            uniqueElemen.push(newArray[i]);
-        }
-    }
-    return uniqueElemen;
+    });
+    return arr;
+   
 
 
 }
-console.log(mergedArray([10,20,30],[20,30 ,40,50]));
+console.log(mergedArray([10,20,30,40],[30,40,50,60,70]));
